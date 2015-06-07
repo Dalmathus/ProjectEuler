8099e0d4422a6d2234f0236ed3e80ad055c8ddb4
Dalmathus <jameswluxton@gmail.com>
2015-06-08 00:37:26 +1200

    Problem 16
    https://projecteuler.net/problem=16

---
+++ Problem 16/Euler16.java
@@ -0,0 +1,28 @@
+import java.math.*;
+
+public class Euler16 {
+
+	/**
+	//	https://projecteuler.net/problem=16
+	//	What is the sum of the digits of the number 21000?
+	//	@author James Luxton
+	**/
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		
+		final int EXPONENT = 1000;
+		BigInteger num = BigInteger.pow(2, EXPONENT);		
+		long sum = 0;
+
+		String[] numbers = num.toString().split("");
+
+		for (String s : numbers) {
+			sum += Integer.parseInt(s);
+		}
+
+		System.out.println(sum);
+
+		long endTime = System.currentTimeMillis();
+		System.err.println("Proccess took " + (endTime - startTime) + " milliseconds");
+	}
+}
